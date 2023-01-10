500ac9367255467b49aa9f8f2958c8c65c969aa4
Cristian Le <cristian.le@mpsd.mpg.de>
2023-01-10 16:45:57 +0100

    Use cmake native install
    Signed-off-by: Cristian Le <cristian.le@mpsd.mpg.de>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cmake ${CMAKE_ARGS} -DCMAKE_C_COMPILER=$CC -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib ${SRC_DIR}
-make install
+cmake --build . --target install
 
 (cd python
     cp -r ../src .
